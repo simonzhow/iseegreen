9f87ac34be2ad2567c371469f4d8f8ebf1a1ec39
Simon Zhou <simon.zhou@ucla.edu>
2017-05-27 10:30:45 -0700

    Working standalone

--- prelim.sh
+++ prelim.sh
@@ -2,3 +2,13 @@
 
 # Preliminary script, will we do this all in shell?
 
+cd /Users/simonzhou/Google\ Drive/Projects/react_tutorial/src
+touch iseegreen.txt
+echo "I love commits!" >> iseegreen.txt
+cd ..
+git init
+git add .
+git commit -m "iseegreen commit"
+git push
+cd src
+rm iseegreen.txt
