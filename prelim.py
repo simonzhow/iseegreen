589bf93f06c306a03f7807b1e721f8532466f889
Simon Zhou <simon.zhou@ucla.edu>
2017-06-21 00:22:38 -0700

    iseegreen

---
+++ prelim.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+
+if (len(sys.argv) != 2):
+	print "ERROR: Invalid number of arguments, please execute the script again"
+	exit()
+	a	
+print subprocess.check_output(['cd', sys.argv[1]])
+print subprocess.check_output(['git','init'])
+print subprocess.check_output(['echo', '1', '>>', '123454321.txt'])
+print subprocess.check_output(['git','add', '.'])
+print subprocess.check_output(['git','commit', '-m', "iseegreen"])
+print subprocess.check_output(['git','push'])
+
+
+
+
+#print "Repository target name is: ", sys.argv[0]
+#print "Number of arguments: ", len(sys.argv)
+#print "The arguments are: " , str(sys.argv)
